ef499a910d5e6b977e79d6db1ff066436f1057da
Benovshe Meherremova <benovshe@Benovshes-MacBook-Pro.local>
2024-01-26 14:12:31 +0400

    delete added

--- main.js
+++ main.js
@@ -9,123 +9,138 @@ let formElement = document.querySelector(".form");
 let submitButton = document.querySelector(".submit");
 let users = [];
 async function fetchData() {
-  try {
-    const response = await fetch(myUrl);
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    const data = await response.json();
+    try {
+      const response = await fetch(myUrl);
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      const data = await response.json();
 
-    data.forEach((e) => {
-      const trow = document.createElement("tr");
-      // console.log(e);
-      trow.innerHTML = `
-        <th scope="row">${e.id}</th>
-        <td>${e.name}</td>
-        <td>${e.surname}</td>
-        <td>${e.number}</td>
-        <td>${e.mail}</td>
-        <td><button type="button" class="btn btn-danger times"><i class="fa-solid fa-xmark"></i></button>
-        <button type="button" class="btn btn-warning update"><i class="fa-solid fa-pen"></i></button></td>
+      data.forEach((e) => {
+        const trow = document.createElement("tr");
+        trow.innerHTML = `
+          <td>${e.name}</td>
+          <td>${e.surname}</td>
+          <td>${e.number}</td>
+          <td>${e.mail}</td>
+          <td>
+            <button type="button" class="btn btn-danger times" data-id="${e.id}">
+              <i class="fa-solid fa-xmark"></i>
+            </button>
+            <button type="button" class="btn btn-warning update" data-id="${e.id}">
+              <i class="fa-solid fa-pen"></i>
+            </button>
+          </td>
         `;
+        tbody.appendChild(trow);
+      });
+
+      searchInput.addEventListener("keyup", (e) => {
+        var val = e.target.value;
+        const filteredData = data.filter((u) => {
+          let name = u.name.toLowerCase();
+          let surname = u.surname.toLowerCase();
+          let mail = u.mail.toLowerCase();
+          return (
+            name.includes(val) ||
+            surname.includes(val) ||
+            mail.includes(val) ||
+            u.number.includes(val)
+          );
+        });
+        filteredTable(filteredData);
+      });
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }
+
+  function filteredTable(filteredData) {
+    tbody.innerHTML = "";
+    filteredData.map((e) => {
+      const trow = document.createElement("tr");
+      trow.innerHTML = `
+          <td>${e.name}</td>
+          <td>${e.surname}</td>
+          <td>${e.number}</td>
+          <td>${e.mail}</td>
+          `;
       tbody.appendChild(trow);
     });
-    searchInput.addEventListener("keyup", (e) => {
-      var val = e.target.value;
-      const filteredData = data.filter((u) => {
-        let name = u.name.toLowerCase();
-        let surname = u.surname.toLowerCase();
-        let mail = u.mail.toLowerCase();
-        return (
-          name.includes(val) ||
-          surname.includes(val || mail.includes(val)) ||
-          u.number.includes(val)
-        );
-      });
-      filteredTable(filteredData);
-    });
-  } catch (error) {
-    console.error("Error fetching data:", error);
   }
-}
-function filteredTable(filteredData) {
-  tbody.innerHTML = "";
-  filteredData.map((e) => {
-    const trow = document.createElement("tr");
-    // console.log(e);
-    trow.innerHTML = `
-        <th scope="row">${e.id}</th>
-        <td>${e.name}</td>
-        <td>${e.surname}</td>
-        <td>${e.number}</td>
-        <td>${e.mail}</td>
-        `;
-    tbody.appendChild(trow);
+
+  formElement.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let nameVal = nameInput.value;
+    let surnameVal = surnameInput.value;
+    let mailVal = emailInput.value;
+    let numberVal = numberInput.value;
+
+    const newUser = {
+      name: nameVal,
+      surname: surnameVal,
+      mail: mailVal,
+      number: numberVal,
+    };
+
+    postNewUser(newUser);
   });
-}
 
-formElement.addEventListener("submit", (e) => {
-  e.preventDefault();
-  let nameVal = nameInput.value;
-  let surnameVal = surnameInput.value;
-  let mailVal = emailInput.value;
-  let numberVal = numberInput.value;
-  const newUser = {
-    id: users.length,
-    name: nameVal,
-    surname: surnameVal,
-    mail: mailVal,
-    number: numberVal,
-  };
-  users.push(newUser);
-  postNewUser(newUser);
-});
+  function postNewUser(newUser) {
+    fetch(myUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newUser),
+    })
+      .then((response) => response.json())
+      .then((responseData) => {
+        newUser.id = responseData.id;
+        users.push(newUser);
+        nameInput.value = "";
+        surnameInput.value = "";
+        emailInput.value = "";
+        numberInput.value = "";
+        fetchData();
+      })
+      .catch((error) => {
+        console.error("Error posting new user:", error);
+      });
+  }
 
-function postNewUser(newUser) {
-  fetch(myUrl, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(newUser),
-  })
-    .then((response) => response.json())
-    .then((responseData) => {
-      newUser.id = responseData.id;
+  document.addEventListener("click", (e) => {
+    if (e.target.classList.contains("times")) {
+      const userId = e.target.getAttribute("data-id");
+      deleteUser(userId);
+    }
+  });
 
-      users.push(newUser);
+  async function deleteUser(userId) {
+    try {
+      await fetch(`${myUrl}/${userId}`, {
+        method: "DELETE",
+      });
 
-      nameInput.value = "";
-      surnameInput.value = "";
-      emailInput.value = "";
-      numberInput.value = "";
+      users = users.filter((user) => user.id !== parseInt(userId));
 
       fetchData();
-    })
-    .catch((error) => {
-      console.error("Error posting new user:", error);
-    });
-}
-
-document.addEventListener("click", (e) => {
-  if (e.target.classList.contains("times")) {
-    const userId = e.target.closest("tr").querySelector("th").textContent;
-    deleteUser(userId);
+    } catch (err) {
+      console.log(err);
+    }
   }
-});
-async function deleteUser(userId) {
-  try {
-    await fetch(`${myUrl}/${userId}`, {
-      method: "DELETE",
-    });
-    console.log(users);
-    users = users.filter((user) => {
-      user.id !== parseInt(userId);
-    });
-    fetchData();
-  } catch (err) {
-    console.log(err);
-  }
+  async function updateUser(updatedId){
+    try {
+        const response = await fetch(`${myUrl}/${updatedId}`,{
+            method: "PUT",
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify(updateTask)
+        })
+        console.log(response);
+    } catch (error) {
+        
+    }
 }
-
-fetchData();
+  fetchData();
